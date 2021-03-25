e4abb59fa7e44290f7e3b0897ee060a7219bcb13
Pere Frontera <pere.frontera@gmail.com>
2021-03-25 23:25:53 +0100

    Fix serilog

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Serilog;
 
 namespace NetCoreSnippets
 {
@@ -15,6 +16,9 @@ namespace NetCoreSnippets
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
         }
 
         public IConfiguration Configuration { get; }
